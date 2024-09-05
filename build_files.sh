ca2609fcf9659a6a4e72caf007ab46d2e7b3253d
unknown <princeuruakpa@gmail.com>
2024-09-05 19:01:51 +0100

    added host tools

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-# Install Python and pip if they are not available
-if ! command -v python3 &> /dev/null; then
-    apt-get update && apt-get install -y python3 python3-pip
-fi
-
-
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic
