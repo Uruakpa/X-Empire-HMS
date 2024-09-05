c1de2dca37b6281fea020eb9d2962e2c0dfaa2df
unknown <princeuruakpa@gmail.com>
2024-09-05 20:16:39 +0100

    added host tools

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-python3 -m pip install -r requirements.txt  # Explicitly using python3
-# python3 manage.py collectstatic --noinput
-python3 manage.py collectstatic --noinput
 
-python3 manage.py migrate
+set -o errexit
+
+# python3 -m 
+pip install -r requirements.txt  # Explicitly using python3
+# python3 manage.py collectstatic --noinput
+# python3 
+manage.py collectstatic --no-input
+
+# python3 
+manage.py migrate
