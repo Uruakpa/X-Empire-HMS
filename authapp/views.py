bfba881954ca06265947759ef4e9663aafaa3d38
Junior's signature <chibuzor.john.2018@gmail.com>
2024-09-10 17:57:35 +0100

    junior

--- authapp/views.py
+++ authapp/views.py
@@ -164,7 +164,7 @@ def login_page(request):
         return redirect('home')
     else:
         if request.method == "POST":
-            username = request.POST.get('email')
+            username = request.POST.get('username')
             password = request.POST.get('password')
 
             user = authenticate(request, username=username, password=password)
