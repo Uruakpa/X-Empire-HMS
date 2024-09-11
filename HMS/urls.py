82708295b723b37c9554b63b4123ee022c723068
Junior's signature <chibuzor.john.2018@gmail.com>
2024-09-11 12:15:04 +0100

    added staff views

--- HMS/urls.py
+++ HMS/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin-site/', django_admin.site.urls),
     path('', include("hotel.urls")),
     path('auth/', include("authapp.urls")),
+    path('staff/', include("staff.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- staff/views.py
+++ staff/views.py
@@ -1,23 +1,48 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 # Create your views here.
 
-def menuitem_views(request):
+@login_required(login_url='login')
+def menuitem_views(request, pk):
     role = str(request.user.groups.all()[0])
     path = role + "/"
+    
+    user = User.objects.get(id=pk)
     
     menuitem = MenuItem.objects.all()
     context = {"menuitem":menuitem}
     return render(request, path + "menuitems.html", context)
 
-def menu_views(request):
+@login_required(login_url='login')
+def menu_views(request,pk):
     role = str(request.user.groups.all()[0])
     path = role + "/"
-    
+    user = User.objects.get(id=pk)
     menu = Menu.objects.all()
     context = {
         "role":role,
         "menu":menu
     }
+    return render(request, path + "menu.html", context)
+
+@login_required(login_url='login')
+def menulist(request, foo,pk):
+    role = str(request.user.groups.all()[0])
+    path = role + "/"
+    user = User.objects.get(id=pk)
+    foo = foo.replace('-', '')
+    menu = Menu.objects.get(name=foo)
+    menuitems = MenuItem.objects.filter(menu=menu)
+    
+    context = {
+        "menu":menu,
+        "menuitems":menuitems
+    }
     return render(request, path + "menulist.html", context)
 
+
+    
+    
