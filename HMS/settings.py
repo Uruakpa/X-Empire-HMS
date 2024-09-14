6297d12f2bf773336f55e7c59bf5c563dc81c333
unknown <princeuruakpa@gmail.com>
2024-09-14 16:37:14 +0100

    set db default

--- HMS/settings.py
+++ HMS/settings.py
@@ -101,18 +101,11 @@ WSGI_APPLICATION = 'HMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if not DEBUG:
-    DATABASES = {
-	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+
+DATABASES = {
+"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-    
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
    
     
 
